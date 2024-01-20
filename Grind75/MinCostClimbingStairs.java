56c4d0ea4c7c101c22ebe5476998ec08ca8acda4
evachen111 <ssevachen@gmail.com>
2024-01-20 01:03:31 +0100

    daily commit

--- Grind75/MinCostClimbingStairs.java
+++ Grind75/MinCostClimbingStairs.java
@@ -1,4 +1,21 @@
 package Grind75;
 
+// 746
 public class MinCostClimbingStairs {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[3];
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i<=cost.length; i++){
+            dp[2] = Math.min(dp[0] + cost[i-1], dp[1] + cost[i-2]);
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+        }
+        return dp[2];
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
